1d7723a4ab8efb33e7572876bd31bf9e34cc0ec1
Luiz Fernando Souza Luz <70658918+luizfernando608@users.noreply.github.com>
2022-06-04 19:51:05 -0300

    Script to generate fake data

---
+++ PubSubscribe/generate_fake_scenario.py
@@ -0,0 +1,29 @@
+#%%
+import consumer
+import numpy as np
+
+#%%
+
+for i in range(1,100):
+    status = np.random.choice(["running","finished"],p=[0.7,0.3], size=1)[0]
+    quantidade_total_comida = 10000
+    if status == "running":
+        tempo_execucao = 0
+    if status == "finished":
+        tempo_execucao = np.random.uniform(10,1000)
+    consumer.insert_scenario.delay(status, tempo_execucao, quantidade_total_comida)
+
+
+#%%
+
+for i in range(1,100):
+    quantidade_comida = np.random.randint(0,10000)
+    quantidade_formiga_carregando = np.random.randint(0,10000-quantidade_comida)
+    quantidade_formiga_procurando = np.random.randint(0,10000-quantidade_comida-quantidade_formiga_carregando)
+    maximo_carregado_formiga = np.random.randint(0,quantidade_comida)
+    id_cenario = i
+    consumer.insert_antihill.delay(quantidade_comida,
+                            quantidade_formiga_carregando,
+                            quantidade_formiga_procurando,
+                            maximo_carregado_formiga,
+                            id_cenario)
