5a83c9e2a108e30a8d8f93562ad0b3d07e67f59d
Luiz Fernando Souza Luz <70658918+luizfernando608@users.noreply.github.com>
2022-06-15 17:37:37 -0300

    change consumer folder

--- consumer.py
+++ consumer.py
@@ -20,83 +20,6 @@ MetaData.reflect(meta)
 #%%
 app = Celery('tasks', broker='amqp://localhost')
 
-# @app.task
-# def insert_scenario(status:str, tempo_execucao:float, quantidade_total_comida:int)-> bool:
-#     scenario = meta.tables['cenario']
-#     insert_scenario = (
-#         insert(scenario).
-#         values(
-#             status=status,
-#             tempo_execucao=tempo_execucao,
-#             quantidade_total_comida=quantidade_total_comida
-#         )
-#     )
-#     engine.execute(insert_scenario)
-#     return True
-
-
-
-
-
-# @app.task
-# def insert_antihill(quantidade_comida:int, quantidade_formiga_carregando:int, quantidade_formiga_procurando:int, maximo_carregado_formiga:int, id_cenario:int):
-#     antihill = meta.tables['formigueiro']
-#     insert_antihill= (
-#     insert(antihill).
-#     values(
-#         quantidade_comida=quantidade_comida,
-#         quantidade_formiga_carregando=quantidade_formiga_carregando,
-#         quantidade_formiga_procurando=quantidade_formiga_procurando,
-#         maximo_carregado_formiga=maximo_carregado_formiga,
-#         id_cenario=id_cenario)
-#     )
-#     engine.execute(insert_antihill)
-#     return True
-
-
-# @app.task
-# def update_scenario_status(id_scenario:int ,status:str, tempo_execucao:float=0)->bool:
-#     scenario = meta.tables["cenario"]
-#     update_scenario_status = (
-#         update(scenario).
-#         where(scenario.c.id_cenario == id_scenario).
-#         values(status=status,
-#             tempo_execucao=tempo_execucao)
-#     )
-#     engine.execute(update_scenario_status)
-#     return True
-
-
-# #%%
-# @app.task
-# def update_scenario_food(id_scenario:int, total_comida:int)->bool:
-#     scenario = meta.tables["cenario"]
-#     update_scenario_quantidade_comida = (
-#         update(scenario).
-#         where(scenario.c.id_cenario == id_scenario).
-#         values(quantidade_total_comida=total_comida)
-#     )
-#     engine.execute(
-#         update_scenario_quantidade_comida
-#     )
-#     return True
-
-# #%%
-# @app.task
-# def update_antihill(id_antihill:int,quantidade_comida:int, quantidade_formiga_carregando:int, quantidade_formiga_procurando:int, maximo_carregado_formiga:int, id_cenario:int)->bool:
-#     anthill = meta.tables["formigueiro"]
-#     update_antihill = (
-#         update(anthill).
-#         where(anthill.c.id_formigueiro == id_antihill).
-#         values(
-#             quantidade_comida=quantidade_comida,
-#             quantidade_formiga_carregando=quantidade_formiga_carregando,
-#             quantidade_formiga_procurando=quantidade_formiga_procurando,
-#             maximo_carregado_formiga = maximo_carregado_formiga,
-#             id_cenario=id_cenario
-#         )
-#     )
-#     engine.execute(update_antihill)
 
 #%%
 def insert_scenario(ID:str,ants_quantity:int,total_food:int,map_food:int,elapsed:float,status:str)-> bool:
@@ -227,21 +150,102 @@ executing_data = {'elapsed': 76.73803091049194,
                 'total_food': 151, 
                 'map_food': 148, 
                 'anthill_food': 0, 
-                'ants_info': [{'status': 1, 'total_food': 0}, 
-                              {'status': 0, 'total_food': 0}, 
-                              {'status': 0, 'total_food': 0}, 
-                              {'status': 1, 'total_food': 0}, 
-                              {'status': 0, 'total_food': 0}, 
-                              {'status': 2, 'total_food': 1}, 
-                              {'status': 0, 'total_food': 0}, 
-                              {'status': 2, 'total_food': 1}, 
-                              {'status': 0, 'total_food': 0}, 
-                              {'status': 0, 'total_food': 0}, 
-                              {'status': 1, 'total_food': 0}, 
-                              {'status': 0, 'total_food': 0}, 
-                              {'status': 1, 'total_food': 0}, 
-                              {'status': 0, 'total_food': 0}, {'status': 0, 'total_food': 0}, {'status': 0, 'total_food': 0}, {'status': 1, 'total_food': 0}, {'status': 2, 'total_food': 1}]}
+                'ants_info': [
+                {'status': 1, 'total_food': 0}, 
+                {'status': 0, 'total_food': 0}, 
+                {'status': 0, 'total_food': 0}, 
+                {'status': 1, 'total_food': 0}, 
+                {'status': 0, 'total_food': 0}, 
+                {'status': 2, 'total_food': 1}, 
+                {'status': 0, 'total_food': 0}, 
+                {'status': 2, 'total_food': 1}, 
+                {'status': 0, 'total_food': 0}, 
+                {'status': 0, 'total_food': 0}, 
+                {'status': 1, 'total_food': 0}, 
+                {'status': 0, 'total_food': 0}, 
+                {'status': 1, 'total_food': 0}, 
+                {'status': 0, 'total_food': 0}, 
+                {'status': 0, 'total_food': 0}, 
+                {'status': 0, 'total_food': 0}, 
+                {'status': 1, 'total_food': 0}, 
+                {'status': 2, 'total_food': 1}]}
 
 
 
 
+#%%
+
+
+# @app.task
+# def insert_scenario(status:str, tempo_execucao:float, quantidade_total_comida:int)-> bool:
+#     scenario = meta.tables['cenario']
+#     insert_scenario = (
+#         insert(scenario).
+#         values(
+#             status=status,
+#             tempo_execucao=tempo_execucao,
+#             quantidade_total_comida=quantidade_total_comida
+#         )
+#     )
+#     engine.execute(insert_scenario)
+#     return True
+
+# @app.task
+# def insert_antihill(quantidade_comida:int, quantidade_formiga_carregando:int, quantidade_formiga_procurando:int, maximo_carregado_formiga:int, id_cenario:int):
+#     antihill = meta.tables['formigueiro']
+#     insert_antihill= (
+#     insert(antihill).
+#     values(
+#         quantidade_comida=quantidade_comida,
+#         quantidade_formiga_carregando=quantidade_formiga_carregando,
+#         quantidade_formiga_procurando=quantidade_formiga_procurando,
+#         maximo_carregado_formiga=maximo_carregado_formiga,
+#         id_cenario=id_cenario)
+#     )
+#     engine.execute(insert_antihill)
+#     return True
+
+
+# @app.task
+# def update_scenario_status(id_scenario:int ,status:str, tempo_execucao:float=0)->bool:
+#     scenario = meta.tables["cenario"]
+#     update_scenario_status = (
+#         update(scenario).
+#         where(scenario.c.id_cenario == id_scenario).
+#         values(status=status,
+#             tempo_execucao=tempo_execucao)
+#     )
+#     engine.execute(update_scenario_status)
+#     return True
+
+
+# #%%
+# @app.task
+# def update_scenario_food(id_scenario:int, total_comida:int)->bool:
+#     scenario = meta.tables["cenario"]
+#     update_scenario_quantidade_comida = (
+#         update(scenario).
+#         where(scenario.c.id_cenario == id_scenario).
+#         values(quantidade_total_comida=total_comida)
+#     )
+#     engine.execute(
+#         update_scenario_quantidade_comida
+#     )
+#     return True
+
+# #%%
+# @app.task
+# def update_antihill(id_antihill:int,quantidade_comida:int, quantidade_formiga_carregando:int, quantidade_formiga_procurando:int, maximo_carregado_formiga:int, id_cenario:int)->bool:
+#     anthill = meta.tables["formigueiro"]
+#     update_antihill = (
+#         update(anthill).
+#         where(anthill.c.id_formigueiro == id_antihill).
+#         values(
+#             quantidade_comida=quantidade_comida,
+#             quantidade_formiga_carregando=quantidade_formiga_carregando,
+#             quantidade_formiga_procurando=quantidade_formiga_procurando,
+#             maximo_carregado_formiga = maximo_carregado_formiga,
+#             id_cenario=id_cenario
+#         )
+#     )
+#     engine.execute(update_antihill)
