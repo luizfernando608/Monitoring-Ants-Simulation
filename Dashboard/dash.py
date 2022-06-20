af1221d2a31497778c2918932f59e18c34da6dd3
Carlos Fonseca <carloscof123@gmail.com>
2022-06-20 01:46:33 -0300

    atualiza

--- Dashboard/dash.py
+++ Dashboard/dash.py
@@ -1,6 +1,20 @@
 import streamlit as st
 from time import sleep
 
+def retorna_linha(valores):
+    tamanho=0
+    for a in valores:
+        tamanho += len(str(a))
+    return '|'+str(valores[0])+(73-tamanho)*' '+str(valores[1])+'|'
+
+
+b='|'
+
+
+#Limpar Código
+import os
+os.system('cls')
+ 
 st.set_page_config(page_title='Formiguinhas',
                    page_icon='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/313/ant_1f41c.png',
                    layout="wide")
@@ -53,7 +67,7 @@ else:
     b5.metric('Média de comida carregada',MediaCarregado)
 
     st.markdown('__________')
-
+    
     c = st.container()
 
     c1,l = c.columns([2,3])
@@ -83,6 +97,63 @@ else:
         c2.write(f"- % Formigueiro: {round(CNCFormigueiro/(total_food_A)*100,2)} %")
         c2.write(f"- % Transito: {round(CNCTransito/(total_food_A)*100,2)} %")
         c3.metric('Probabilidade',str(CProb) +' %')
+        print(
+            retorna_linha(['Valores:','']),
+            retorna_linha([' Total Cenários', NCenario]),
+            retorna_linha([' Total Formigueiro',NFormigueiro]),
+            retorna_linha([' Total Formiga',NFormigas]),
+            retorna_linha(['  % Carregando',str(round(NFCarregando/NFormigas*100,2))+'%']),
+            retorna_linha(['  % Procurando',str(round(NFProcurando/NFormigas*100,2))+'%']),
+            retorna_linha([' Total Comida',NComida]),
+            retorna_linha(['  % Fonte',str(round(NCFonte/NComida*100,2))+'%']),
+            retorna_linha(['  % Formigueiro',str(round(NCFormigueiro/NComida*100,2))+'%']),
+            retorna_linha(['  % Transito',str(round(NCTransito/NComida*100,2))+'%']),
+            retorna_linha(['','']),
+            retorna_linha(['Tempo Cenários:','']),
+            retorna_linha([' Máximo',MaxTempo]),
+            retorna_linha(['  Quem',IDMaxTempo]),
+            retorna_linha([' Mínimo',MinTempo]),
+            retorna_linha(['  Quem',IDMinTempo]),
+            retorna_linha([' Média',MediaTempo]),
+            retorna_linha(['Comida Carregada por Formiga:','']),
+            retorna_linha([' Máximo',MaxCarregado]),
+            retorna_linha([' Média', MediaCarregado]),
+            retorna_linha(['','']),
+            retorna_linha(['CENARIO',seletor]),
+            retorna_linha(['Numero Formigueiros',CNFormigueiros]),
+            retorna_linha(['Numero Formigas',CNFormigas]),
+            retorna_linha(['Numero Comida',CNComida]),
+            retorna_linha(['Tempo de Execução',CTempo]),
+            retorna_linha(['FORMIGUEIRO','']),
+            retorna_linha([' Numero Formigas',CNFormigas]),
+            retorna_linha(["- % Carregando" ,f"{round(CNFCarregando/CNFormigas*100,2)} %"]),
+            retorna_linha(["- % Procurando",f"{round(CNFProcurando/CNFormigas*100,2)} %"]),
+            retorna_linha([' Numero Comida', total_food_A]),
+            retorna_linha(["- % Formigueiro",f" {round(CNCFormigueiro/(total_food_A)*100,2)} %"]),
+            retorna_linha(["- % Transito",f"{round(CNCTransito/(total_food_A)*100,2)} %"]),
+            retorna_linha([' Probabilidade',str(CProb) +' %']),sep='\n')
+    else:
+        print(
+        retorna_linha(['Valores:','']),
+        retorna_linha([' Total Cenários', NCenario]),
+        retorna_linha([' Total Formigueiro',NFormigueiro]),
+        retorna_linha([' Total Formiga',NFormigas]),
+        retorna_linha(['  % Carregando',str(round(NFCarregando/NFormigas*100,2))+'%']),
+        retorna_linha(['  % Procurando',str(round(NFProcurando/NFormigas*100,2))+'%']),
+        retorna_linha([' Total Comida',NComida]),
+        retorna_linha(['  % Fonte',str(round(NCFonte/NComida*100,2))+'%']),
+        retorna_linha(['  % Formigueiro',str(round(NCFormigueiro/NComida*100,2))+'%']),
+        retorna_linha(['  % Transito',str(round(NCTransito/NComida*100,2))+'%']),
+        retorna_linha(['','']),
+        retorna_linha(['Tempo Cenários:','']),
+        retorna_linha([' Máximo',MaxTempo]),
+        retorna_linha(['  Quem',IDMaxTempo]),
+        retorna_linha([' Mínimo',MinTempo]),
+        retorna_linha(['  Quem',IDMinTempo]),
+        retorna_linha([' Média',MediaTempo]),
+        retorna_linha(['Comida Carregada por Formiga:','']),
+        retorna_linha([' Máximo',MaxCarregado]),
+        retorna_linha([' Média', MediaCarregado]),sep='\n')
 
 
 # automatically rerun after 30s
